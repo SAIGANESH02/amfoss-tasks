f9d4a349079eb7a6a900ed25c4763a5ea809fbc5
saiganesh2 <72904096+saiganesh2@users.noreply.github.com>
2020-10-15 09:56:18 +0530

    Update 282A.py

--- task-2/Codeforces/282A.py
+++ task-2/Codeforces/282A.py
@@ -8,10 +8,11 @@ for i in range(y):
     a = input()
     
     if b in a:
-        x+=1
+        x=x+1
     
     elif c in a:
-        x-=1
+        x=x-1
+print("answer : ")
 print(x)
     
     
